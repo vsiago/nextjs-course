e7ef1d1129940d957ebbf6b7986c5ea755b27e26
vsiagoCodeCity <iago@codecity.app>
2024-07-05 10:47:44 -0300

    Explaning routes

---
+++ src/app/games/layout.tsx
@@ -0,0 +1,12 @@
+export default function GamesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>Games Layout</h1>
+      {children}
+    </div>
+  );
+}
---
+++ src/app/games/nitendo/page.tsx
@@ -0,0 +1,3 @@
+export default function Nitendo() {
+  return <h1>Nitendo</h1>;
+}
